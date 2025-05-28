5c8c9724dc62da8e81d945a9950bc8785e323320
Ankita Arya <ankita.arya@hiteshi.com>
2025-05-28 20:11:24 +0530

    yml changed

--- src/db/db.js
+++ src/db/db.js
@@ -13,6 +13,7 @@ const sequelize = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
     dialect: 'postgres',
     logging: false,
 });
